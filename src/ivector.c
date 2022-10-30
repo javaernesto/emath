0164f0f382c83e95158b01c14e992169bef99f02
javaernesto <76874271+javaernesto@users.noreply.github.com>
2022-10-30 19:02:46 +0100

    Update source code and add new functions
    Added new source file random.c for random linear algebra (randomsample, random_cycle, ...)

--- src/ivector.c
+++ src/ivector.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+
 #include "../include/ivector.h"
 
 
@@ -91,6 +92,10 @@ void free_ivector(ivector *v) {
 	free(v->val);
 
 	free(v);
+
+	#ifdef FREE
+    printf("FREE IV\n");
+    #endif
 }
 
 /**
@@ -102,9 +107,339 @@ void free_imatrix(imatrix *mat) {
 	
 	for (size_t i = 0; i < mat->n; i++)
 		free(mat->val[i]);
+
 	free(mat->val);
-	
+
 	free(mat);
+
+	#ifdef FREE
+    printf("FREE IM\n");
+    #endif
+}
+
+/**
+ * @brief Auxiliary function for print_ivector
+ * @param v integer vector to print
+ */
+void _print_iv(ivector *v) {
+	
+	printf("[");
+	if (v->n <= 20) {
+		for (size_t i = 0; i < v->n - 1; i++)
+			printf("%ld, ", v->val[i]);
+	} else {
+		for (size_t i = 0; i < 20; i++)
+			printf("%ld, ", v->val[i]);
+		printf( "..., ");
+
+		for (size_t i = v->n - 20; i < v->n - 1; i++)
+			printf("%ld, ", v->val[i]);
+	}
+	printf("%ld]", v->val[v->n - 1]);
+
+}
+
+/**
+ * @brief Auxiliary function for fprint_ivector
+ * @param v integer vector to print
+ */
+void _fprint_iv(FILE *fp, ivector *v) {
+	
+	fprintf(fp, "[");
+	if (v->n <= 20) {
+		for (size_t i = 0; i < v->n - 1; i++)
+			fprintf(fp, "%ld, ", v->val[i]);
+	} else {
+		for (size_t i = 0; i < 20; i++)
+			fprintf(fp, "%ld, ", v->val[i]);
+		fprintf(fp,  "..., ");
+
+		for (size_t i = v->n - 20; i < v->n - 1; i++)
+			fprintf(fp, "%ld, ", v->val[i]);
+	}
+	fprintf(fp, "%ld]", v->val[v->n - 1]);
+
+}
+
+/**
+ * @brief Auxiliary function for print_imatrix
+ * @param val long int array containing row
+ * @param n size of val
+ */
+void _print_ld(long *val, size_t n) {
+
+	printf("[");
+	if (n <= 20) {
+		for (size_t i = 0; i < n - 1; i++)
+			printf("%ld, ", val[i]);
+	} else {
+		for (size_t i = 0; i < 3; i++)
+			printf("%ld, ", val[i]);
+		printf( "..., ");
+
+		for (size_t i = n - 3; i < n - 1; i++)
+			printf("%ld, ", val[i]);
+	}
+	printf("%ld]", val[n - 1]);
+}
+
+/**
+ * @brief Auxiliary function for fprint_imatrix
+ * @param val long int array containing row
+ * @param n size of val
+ */
+void _fprint_ld(FILE *fp, long *val, size_t n) {
+
+	fprintf(fp, "[");
+	if (n <= 20) {
+		for (size_t i = 0; i < n - 1; i++)
+			fprintf(fp, "%ld, ", val[i]);
+	} else {
+		for (size_t i = 0; i < 3; i++)
+			fprintf(fp, "%ld, ", val[i]);
+		fprintf(fp,  "..., ");
+
+		for (size_t i = n - 3; i < n - 1; i++)
+			fprintf(fp, "%ld, ", val[i]);
+	}
+	fprintf(fp, "%ld]", val[n - 1]);
+}
+
+/**
+ * @brief Print ivector (Python numpy library format). If vector size larger 
+ * than 20, print brief representation
+ * @param v integer vector to print
+ */
+void print_ivector(ivector *v) {
+
+	#ifdef DEBUG
+	printf("size %ld\n", v->n);
+	#endif
+
+	printf("vector(");
+	_print_iv(v);
+	printf(")\n");
+}
+
+/**
+ * @brief Print ivector to file (Python numpy library format). 
+ * If vector size larger than 20, print brief representation
+ * @param v integer vector to print
+ */
+void fprint_ivector(FILE *fp, ivector *v) {
+
+	#ifdef DEBUG
+	fprintf(fp, "size %ld\n", v->n);
+	#endif
+
+	fprintf(fp, "vector(");
+	_fprint_iv(fp, v);
+	fprintf(fp, ")\n");
+}
+
+/**
+ * @brief Print imatrix (Python numpy library format). If row size or column 
+ * size larger than 20, print brief representation
+ * @param A integer matrix to print
+ */
+void print_imatrix(imatrix *A) {
+
+	#ifdef DEBUG
+	printf("shape (%ld, %ld)\n", A->n, A->m);
+	#endif
+
+	printf("matrix([");
+	if (A->n <= 20) {
+		_print_ld(A->val[0], A->m);
+		printf(",\n");	
+		for (size_t i = 1; i < A->n - 1; i++) {
+			printf("	");
+			_print_ld(A->val[i], A->m);
+			printf(",\n");
+		}
+		printf("	");
+		_print_ld(A->val[A->n - 1], A->m);
+	} else {
+		_print_ld(A->val[0], A->m);
+		printf(",\n");
+		for (size_t i = 1; i < 3; i++) {
+			printf("	");
+			_print_ld(A->val[i], A->m);
+			printf(",\n");
+		}
+		printf( "	...,\n");
+
+		for (size_t i = A->n - 3; i < A->n - 1; i++) {
+			printf("	");
+			_print_ld(A->val[i], A->m);
+			printf(",\n");
+		}
+		printf("	");
+		_print_ld(A->val[A->n - 1], A->m);
+	}
+	printf("])\n");
+}
+
+/**
+ * @brief Print imatrix to file (Python numpy library format). 
+ * If row size or column size larger than 20, print brief representation
+ * @param A integer matrix to print
+ */
+void fprint_imatrix(FILE *fp, imatrix *A) {
+
+	#ifdef DEBUG
+	fprintf(fp, "shape (%ld, %ld)\n", A->n, A->m);
+	#endif
+
+	fprintf(fp, "matrix([");
+	if (A->n <= 20) {
+		_fprint_ld(fp,A->val[0], A->m);
+		fprintf(fp, ",\n");	
+		for (size_t i = 1; i < A->n - 1; i++) {
+			fprintf(fp, "	");
+			_fprint_ld(fp,A->val[i], A->m);
+			fprintf(fp, ",\n");
+		}
+		fprintf(fp, "	");
+		_fprint_ld(fp,A->val[A->n - 1], A->m);
+	} else {
+		_fprint_ld(fp,A->val[0], A->m);
+		fprintf(fp, ",\n");
+		for (size_t i = 1; i < 3; i++) {
+			fprintf(fp, "	");
+			_fprint_ld(fp,A->val[i], A->m);
+			fprintf(fp, ",\n");
+		}
+		fprintf(fp,  "	...,\n");
+
+		for (size_t i = A->n - 3; i < A->n - 1; i++) {
+			fprintf(fp, "	");
+			_fprint_ld(fp,A->val[i], A->m);
+			fprintf(fp, ",\n");
+		}
+		fprintf(fp, "	");
+		_fprint_ld(fp,A->val[A->n - 1], A->m);
+	}
+	fprintf(fp, "])\n");
+}
+
+/**
+ * @brief Read csv or dat file (one column file only, use for structure.dat)
+ * @param filename name of csv or dat file to read
+ * @return imatrix* 
+ */
+imatrix *read_icsv(char *filename) {
+
+	FILE *fp;
+	char line[1024];
+	size_t n = 0, m = 1;
+	size_t j = 0;
+	int counter = 0;
+    imatrix *output;
+
+	fp = fopen(filename, "r");
+ 
+    if (!fp) {
+        printf("Can't open file\n");
+        
+        return NULL;
+    } else {
+
+		#ifdef DEBUG
+		printf("\rRead file %s", filename);
+		fflush(stdout);
+		#endif
+		
+		while (fgets(line, sizeof(line), fp)) {
+			n++;
+		}
+
+		/* Find number of separators to deduce number of columns */
+		// TODO: Error handling. Inconsistent number of columns
+		while(line[j] != '\0') {
+			if(line[j] == ',')
+				counter++;
+			j++;
+		}
+
+		m = counter + 1;
+				
+		/* Reset pointer to beginning of file after reading number of
+		 * lines */
+		fseek(fp, 0, SEEK_SET);
+
+		output = init_imatrix(n, m);
+				
+		for (size_t i = 0; i < n; i++) {
+            for (size_t j = 0; j < m; j++)
+			    fscanf(fp, "%ld%*[,\n]", &output->val[i][j]);
+		}
+		
+		fclose(fp);
+		
+		#ifdef DEBUG
+		printf("\r[ok]Read file %s\n", filename);
+		#endif
+
+		return output;
+	}
+}
+
+/**
+ * @brief Print integer vector to file
+ * @param v integer vector to save in file
+ * @param filename name of file
+ * @return int 0 if OK, 1 if cannot open file
+ */
+int ivtofile(ivector *v, const char *filename) {
+
+	FILE *fp;
+	size_t i = 0;
+
+	fp = fopen(filename, "w");
+
+	if (!fp) {
+		printf("Cannot open file\n");
+
+		return 1;
+	} else {
+		for (i = 0; i < v->n; i++)
+			fprintf(fp, "%ld\n", v->val[i]);
+
+		fclose(fp);
+
+		return 0;
+	}
+}
+
+/**
+ * @brief Print integer matrix to file
+ * @param v integer matrix to save in file
+ * @param filename name of file
+ * @return int 0 if OK, 1 if cannot open file
+ */
+int imtofile(imatrix *M, const char *filename) {
+
+	FILE *fp;
+	size_t i = 0, j = 0;
+
+	fp = fopen(filename, "w");
+
+	if (!fp) {
+		printf("Cannot open file\n");
+
+		return 1;
+	} else {
+		for (i = 0; i < M->n; i++) {
+			for (j = 0; j < M->m - 1; j++)
+				fprintf(fp, "%ld,", M->val[i][j]);
+			fprintf(fp, "%ld\n", M->val[i][j]);
+		}
+
+		fclose(fp);
+
+		return 0;
+	}
 }
 
 /**
@@ -119,6 +454,27 @@ ivector *to_ivector(long *vals, size_t n) {
 
 	memcpy(output->val, vals, n * sizeof(long));
 	
+	return output;
+}
+
+/**
+ * @brief Compute matrix of absolute values
+ * @param A integer matrix
+ * @return imatrix* 
+ */
+imatrix *imabs(imatrix *A) {
+
+	size_t i = 0, j = 0;
+	imatrix *output;
+
+	output = init_imatrix(A->n, A->m);
+
+	for (i = 0; i < A->n; i++) {
+		for (j = 0; j < A->m; j++) {
+			output->val[i][j] = labs(A->val[i][j]);
+		}
+	}
+
 	return output;
 }
 
@@ -174,7 +530,7 @@ double imnorm(imatrix *mat, unsigned int p) {
 	double sz 	= 0.0;
 	double norm = 0.0;
 
-	switch(p) {
+	switch (p) {
 		case 0: // p = Inf -> max rows
 			for (size_t i = 0; i < mat->n; i++) {
 				for (size_t j = 0; j < mat->m; j++) {
@@ -204,6 +560,44 @@ double imnorm(imatrix *mat, unsigned int p) {
 	}
 	
 	return norm;
+}
+
+/**
+ * @brief Compute p-distance between matrix A and matrix B
+ * @param A integer matrix
+ * @param B integer matrix
+ * @param p 0 < p <= Inf such that we get ||A - B||_p (enter 0 for p = Inf)
+ * @return double 
+ */
+double imdist(imatrix *A, imatrix *B, unsigned int p) {
+
+	double dist = 0;
+	imatrix *tmp, *C;
+
+	/* Compute A - B first and then |A - B| */
+	tmp = imsub(A, B);
+	C = imabs(tmp);
+	dist = imnorm(C, p);
+
+	free_imatrix(tmp);
+	free_imatrix(C);
+
+	return dist;
+}
+
+/**
+ * @brief Return integer vector of size n of increasing numbers, starting 0
+ * @param n size of vecto
+ * @return *ivector 
+ */
+ivector *iarange(size_t n) {
+
+	ivector *v = init_ivector(n);
+
+	for (size_t i = 0; i < n; i++)
+        (v->val)[i] = i;
+
+	return v;
 }
 
 /**
@@ -291,6 +685,25 @@ ivector *ivadd(ivector *a, ivector *b) {
 }
 
 /**
+ * @brief return vector substraction of two vectors
+ * @param a integer vector a
+ * @param b integer vector b
+ * @return *ivector
+ */
+ivector *ivsub(ivector *a, ivector *b) {
+
+    // Assert ivector have same size
+	assert(a->n == b->n);
+
+    ivector *c = init_ivector(a->n);
+
+    for (size_t i = 0; i < c->n; i++)
+        (c->val)[i] = (a->val)[i] - (b->val)[i];
+
+    return c;
+}
+
+/**
  * @brief return matrix addition of two integer matrices
  * @param A integer matrix A
  * @param B integer matrix B
@@ -310,6 +723,82 @@ imatrix *imadd(imatrix *A, imatrix *B) {
     }
 
     return C;
+}
+
+/**
+ * @brief return matrix substraction of two integer matrices
+ * @param A integer matrix A
+ * @param B integer matrix B
+ * @return *imatrix
+ */
+imatrix *imsub(imatrix *A, imatrix *B) {
+
+    // Assert imatrix have same dimensions
+	assert((A->n == B->n) && (A->m == B->m));
+
+    imatrix *C = init_imatrix(A->n, A->m);
+
+    for (size_t i = 0; i < C->n; i++) {
+        for (size_t j = 0; j < C->m; j++) {
+            (((C->val)[i]))[j] = (((A->val)[i]))[j] - (((B->val)[i]))[j];
+        }
+    }
+
+    return C;
+}
+
+/**
+ * @brief Compute coordinates of maximum element in integer matrix A
+ * @param A integer matrix
+ * @return size_t* 
+ */
+size_t *immax(imatrix *A) {
+
+	double counter = 0;
+	
+	size_t *coor = calloc(2, sizeof(size_t));
+
+	for (int i = 0; i < A->n; i++) {
+		for (int j = 0; j < A->m; j++) {
+			if (A->val[i][j] > counter) {
+				counter = A->val[i][j];
+				coor[0] = i;
+				coor[1] = j;
+			}
+		}
+	}
+	
+	return coor;
+}
+
+/**
+ * @brief Find element candidate in vector v and return vector of same size 
+ * containing 1 if candidate is in position i and 0 otherwise
+ * @param v integer vector to search
+ * @param candidate element to search in vector
+ * @return ivector* 
+ */
+ivector *ivfind(ivector *v, long candidate) {
+
+	size_t k = 0, counter = 0;
+	ivector *indices;
+
+	indices = init_ivector(v->n);
+	 
+	for (k = 0; k < v->n; k++) {
+		if (v->val[k] == candidate) {
+			indices->val[k] = 1;
+			counter++;		
+		} else {
+			indices->val[k] = 0;
+		}
+	}
+
+	#ifdef DEBUG
+	printf("Found %ld / %ld\n", counter, v->n);
+	#endif
+	
+	return indices;
 }
 
 /**
@@ -350,7 +839,7 @@ imatrix *immul(imatrix *A, imatrix *B) {
             for (size_t k = 0; k < A->m; k++) {
                 c_ij += (((A->val)[i]))[k] * (((B->val)[k]))[j];
             }
-            (((C->val)[i]))[i] = c_ij;
+            (((C->val)[i]))[j] = c_ij;
             c_ij = 0;
         }
     }
@@ -358,41 +847,67 @@ imatrix *immul(imatrix *A, imatrix *B) {
     return C;
 }
 
-/*
+/**
+ * @brief Sum coordinates of vector v between coordinate a and b 
+ * (inclusive)
+ * @param v integer vector to sum
+ * @param a lower coordinate
+ * @param b upper coordinate
+ * @return long int
+ */
+long int isum(ivector *v, size_t a, size_t b) {
 
-int main() {
+	// Assert coordinates are in range
+	assert(((a >= 0) && (b < v->n)) && (a <= b));
+	
+	if (a == b)
+		return 0;
 
-    // TESTING
+	long int s = 0;
+	
+	for (long int i = a; i <= b; i++)
+		s += v->val[i];
+		
+	return s;
+}
 
-	long v1[5] = {8, 2, 3, 4, 5};
-	long v2[5] = {3, 0, 4, 0, 0};
+/**
+ * @brief Transpose integer matrix A
+ * @param A integer matrix to transpose
+ * @return imatrix*
+ */
+imatrix *itranspose(imatrix *A) {
 
-	ivector *a = to_ivector(v1, 5);
-	ivector *b = to_ivector(v2, 5);
+	imatrix *B = init_imatrix(A->m, A->n);
 
-    ivector *c = ivadd(a, b);
-
-	printf("vdot %ld \n", vdot(a, b));
-
-    for (size_t i = 0; i < c->n; i++) {
-        printf("%ld ", (c->val)[i]);
-    }
-    printf("\n");
-
-	imatrix *mat = diag(a);
-	for (size_t i = 0; i < mat->n; i++) {
-		for (size_t j = 0; j < mat->m; j++)
-			printf("%ld ", (((mat->val)[i]))[j]);
-		printf("\n");
+	for (size_t i = 0; i < A->n; i++) {
+		for (size_t j = 0; j < A->m; j++) {
+			B->val[i][j] = A->val[j][i];
+		}
 	}
 
-    printf("norm a = %.3f\n", ivnorm(a, 0));
-    printf("norm b = %.3f\n", ivnorm(b, 2));
+	return B;
+}
 
-	free_ivector(a);
-    free_ivector(b);
-    free_ivector(c);
-	free_imatrix(mat);
+/**
+ * @brief Transform single-column integer matrix to an integer vector. Free 
+ * matrix pointer.
+ * @param A matrix to vectorize
+ * @return *ivector
+ */
+ivector *ivectorize(imatrix *A) {
 
-	return 0;
-} */
+	ivector *output;
+
+	// Check matrix has one column only
+	assert(A->m == 1);
+
+	output = init_ivector(A->n);
+
+	for (size_t i = 0; i < A->n; i++)
+		output->val[i] = A->val[i][0];
+
+	free_imatrix(A);
+
+	return output;
+}
